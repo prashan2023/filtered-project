b8d8722a68ca5bed04fe8cf793e0c96d301c2a38
prasak <prashanramachandran123@gmail.com>
2025-07-05 15:23:25 +0530

    create a navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-
+import { CiHeart  } from "react-icons/ci";
+import { PiShoppingCartSimpleThin } from "react-icons/pi";
+import { HiOutlineUserPlus } from "react-icons/hi2";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-     <div className="text-center">
-        Navbar
+     <div className="flex justify-between border-black border-b-[1px] mt-10 ml-64">
+       <div>
+        <input 
+         className="border-black border-[1px] mb-5 px-4 py-2"
+         type="text"
+         placeholder="Enter the search shoes"
+        />
+       </div>
+       <div>
+          <ul className="flex mr-30">
+            <li className="px-5">
+              <CiHeart className="size-10"/>
+            </li>
+            <li className="px-5">
+              <PiShoppingCartSimpleThin className="size-10"/>
+            </li>
+            <li className="px-5">
+              <AiOutlineUserAdd className="size-10 "/>
+            </li>
+       </ul>
+       </div>
      </div>
   )
 }
