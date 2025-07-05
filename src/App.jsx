6e66e20cb1cdf354554a4f1ee213122781e3d07b
prasak <prashanramachandran123@gmail.com>
2025-07-05 13:37:09 +0530

    setup filtered project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
+
+const App =()=>{
+  return(
+    <>
+    <Navbar/>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default App;
